06ca9a471d5c277e951116d177502b4d2708e810
Nitsan BenHanoch <nitsanbh@gmail.com>
2024-08-09 06:57:31 +0300

    .

--- src/main.c
+++ src/main.c
@@ -1,98 +1,10 @@
-// #include "graph.h"
-
-// #include <stdbool.h>
-// #include <stdio.h>
-// #include <stdlib.h>
-
-// #define D 0.15 // damping factor
-
-
-// void PageRank(Graph *graph, size_t iterations, float* ranks) {
-//   size_t N = graph->num_vertices;
-
-//   for (size_t i = 0; i < N; i++) {
-//     ranks[i] = 1.0 / N;
-//   }
-
-//   float *newRanks = (float *) malloc(N * sizeof(float));
-
-//   bool *does_link_to = (bool *) calloc(N * N, sizeof(bool));
-
-//   for (size_t i = 0; i < N; i++) {
-//     for (size_t j = 0; j < N; j++) {
-//       if (i == j) continue;
-
-//       for (size_t k = 0; k < graph->neighbors_of[j].size; ++k) {
-//         if (graph->neighbors_of[j].data[k] == i) {
-//           does_link_to[j * N + i] = 1;
-//           break;
-//         }
-//       }
-//     }
-//   }
-  
-//   for (size_t iter = 0; iter < iterations; iter++) {
-
-//     // calculate nodes with outlinks to i
-
-//     for (size_t i = 0; i < N; i++) {
-      
-//       // calculate i rank
-
-//       double sumA = 0.0;
-//       double sumB = 0.0;
-
-//       for (size_t j = 0 ; j < N; j++) {
-//         if (does_link_to[j * N + i] == 1) {
-//           sumA += ranks[j] / graph->neighbors_of[j].size;
-//         } else if (graph->neighbors_of[j].size == 0) {
-//           sumB += ranks[j]/N;
-//         }
-//       }
-
-//       newRanks[i] = D/N +(1-D)*(sumA+sumB);
-//     }
-
-//     for (size_t i = 0; i < N; i++) {
-//       ranks[i] = newRanks[i];
-//     }
-//   }
-
-//   free(newRanks);
-// }
-
-// int main(void) {
-//   size_t NUM_NODES = 3;
-//   size_t iterations = 100;
-
-//   Graph *graph = createGraph(NUM_NODES);
-
-//   addEdge(graph, 0, 1);
-//   addEdge(graph, 0, 2);
-//   addEdge(graph, 1, 0);
-//   addEdge(graph, 1, 2);
-//   addEdge(graph, 2, 0);
-
-
-//   float *ranks = (float *) malloc(NUM_NODES * sizeof(float));
-
-//   PageRank(graph, iterations, ranks);
-
-//   for (size_t i = 0; i < NUM_NODES; i++) {
-//     printf("Rank of node %lu: %f\n", i, ranks[i]);
-//   }
-
-//   return 0;
-// }
-
 #include "pagerank.h"
 
 #include <stdio.h>
 #include <stdlib.h>
 
 int main() {
-  size_t N = 1024 * 1024 * 1024;
-  // size_t N = 13;
+  size_t N = 2 * 1024;
   Graph *g = createGraph(N);
 
   float *ranks = (float *) malloc(N * sizeof(float));
--- src/pagerank.c
+++ src/pagerank.c
@@ -3,6 +3,7 @@
 #include "threadpool.h"
 
 #include <assert.h>
+#include <stdbool.h>
 
 static threadpool pool = NULL;
 static const int NUM_WORKERS = 4;
@@ -58,20 +59,27 @@ void PageRank(Graph *graph, int iterations, float *output_ranks) {
     else PANIC("Could not create thread pool");
   }
 
-  START_TIMER(loop);
+  // At first, I parallelized this memset loop. Then I tested it. 
+  // For N = 2 * 1024 * 1024, memset takes 0.0006 seconds, and parallel_memset takes 0.0003 seconds.
+  // This seemed like a vicotry! However,
+  // For N = 1024 * 1024 * 1024, memset takes 0.41 seconds, and parallel_memset takes 0.95 seconds.
+  // For larger N values, the problem steepens. I investigated it online. 
+  // It seems that usually MEMSET ISN'T WORTH PARALLELIZING. THE ISSUE SEEMS TO BE THE MEMORY BUS.
+  // And, we're talking sub-seconds for GBs of data, which is the the upper limit of RAM size anyway.
   for (size_t i = 0; i < N; i++) {
-    output_ranks[i] = 14.7f;
-  }
-  PRINT_TIMER(loop);
-
-  START_TIMER(func);
-  fill_array_with(output_ranks, N, 155.6f);
-  PRINT_TIMER(func);
-
-  for (size_t i = 0; i < N; i++) {
-    assert(output_ranks[i] == 155.6f);
+    output_ranks[i] = 1.0 / N;
   }
 
-  if (iterations == 7 && output_ranks == NULL)
-    ;
+  float *next_ranks = (float *) malloc(N * sizeof(float));
+
+  if (!next_ranks) PANIC("malloc failed");
+
+  bool *has_link = (bool *) calloc(N * N, sizeof(bool));
+  
+  for (size_t i = 0; i < N; i++) {
+    for (size_t edge = 0; edge < graph->neighbors_of[i].size; ++edge) {
+      size_t j = graph->neighbors_of[i].data[edge];
+      has_link[i * N + j] = 1;
+    }
+  }
 }
